bf111e36760722bf202cf1cd54c7aac197797d5e
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-07-22 08:48:58 -0500

    Add files via upload

---
+++ scripts/deployArgs.js
@@ -0,0 +1,26 @@
+module.exports = [
+ 'Sloth Club NFT',
+ 'SCNFT',
+ 
+    '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
+    '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
+    'https://public-pre-ipfs.s3.amazonaws.com/Sloth_Club_NFT/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/Sloth_Club_NFT/assets/SlothReveal.json',
+
+    [
+        ethers.utils.getAddress('0xB784269721dA07b7BABEbC9db30c0d982B1f0964'),
+        ethers.utils.getAddress('0x26E5cBCae6F24264Afb50f603C18c9Ea84C4a5e6'),
+        ethers.utils.getAddress('0xaDB9DEC1a42037310A1Af0F335e43F8303246376'),
+        ethers.utils.getAddress('0xb7be001B3f1ed0191E86bde87394e72d28C5B8D7'),
+        ethers.utils.getAddress('0xc1Dc9C590Af6745d99767BfA94d0e8718E9BBF69'),
+        ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989')
+    ],
+    [
+        40,
+        10,
+        5,
+        5,
+        20,
+        20
+    ]
+]
